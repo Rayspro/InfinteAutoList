f527f4fb8828f5f5b1e56fed87cd95780fc5e2f9
EduGorilla Community <ravi@edugorilla.com>
2019-05-16 12:51:02 +0530

    code

--- infiniteautolist/src/main/java/com/anonymousx/infiniteautolist/InfiniteAutoList.java
+++ infiniteautolist/src/main/java/com/anonymousx/infiniteautolist/InfiniteAutoList.java
@@ -1,4 +1,11 @@
 package com.anonymousx.infiniteautolist;
 
+import android.content.Context;
+import android.util.Log;
+
 public class InfiniteAutoList {
+
+    public InfiniteAutoList(Context context,String text) {
+        Log.d("log","text");
+    }
 }
